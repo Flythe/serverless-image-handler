87c7cfd7605b9b56f5ff96ebca16c1c4553354d6
Allard van Altena <Flythe1+github@gmail.com>
2019-11-05 22:36:16 +0100

    Update build script
    Automatically moves packaged code to S3 bucket, can also auto-deploy a stack or auto-update a lambda function.

--- deployment/build-s3-dist.sh
+++ deployment/build-s3-dist.sh
@@ -1,3 +1,8 @@
+# param 1: S3 bucket name
+# param 2: version number
+# (optional) param 3: lambda function name OR stack name
+# (optional) param 4: when creating a stack this is the source S3 bucket for the images
+
 echo "------------------------------------------------------------------------------"
 echo "Setup the dist folder"
 echo "------------------------------------------------------------------------------"
@@ -10,10 +15,10 @@ echo "--------------------------------------------------------------------------
 cp *.template dist/
 
 replace="s/%%BUCKET_NAME%%/$1/g"
-sed -i '' -e $replace dist/*.template
+sed -i -e $replace dist/*.template
 
-replace="s/%%VERSION%%/$3/g"
-sed -i '' -e $replace dist/*.template
+replace="s/%%VERSION%%/$2/g"
+sed -i -e $replace dist/*.template
 
 cd ../source
 
@@ -23,3 +28,30 @@ echo "--------------------------------------------------------------------------
 npm install
 npm run build
 cp dist/image-handler.zip ../deployment/dist/image-handler.zip
+
+cd ../deployment
+
+echo "------------------------------------------------------------------------------"
+echo "Upload to S3 bucket"
+echo "------------------------------------------------------------------------------"
+aws s3 cp ./dist/ s3://$1/$2/ --recursive
+
+## Uncomment if you want to create a stack automatically
+# echo "------------------------------------------------------------------------------"
+# echo "Creating the stack"
+# echo "------------------------------------------------------------------------------"
+# aws cloudformation create-stack \
+#     --stack-name $3 \
+#     --capabilities CAPABILITY_NAMED_IAM\
+#     --template-url https://s3.amazonaws.com/$1/$2/serverless-image-handler.template \
+#     --parameters ParameterKey=SourceBuckets,ParameterValue=$4
+
+## Uncomment if you want to publish code directly to your lambda function
+# echo "------------------------------------------------------------------------------"
+# echo "Updating lambda function"
+# echo "------------------------------------------------------------------------------"
+# aws lambda update-function-code \
+#     --function-name $3 \
+#     --s3-bucket $1 \
+#     --s3-key $2/image-handler.zip \
+#     --publish
